df17bb3ddd52de99bac203639e5ed4579ea52ae6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-08 12:28:55 +0100

    ft: add the auth service interface

--- atheneum-app/atheneum-app/DataAccess/Interfaces/IAuthService.cs
+++ atheneum-app/atheneum-app/DataAccess/Interfaces/IAuthService.cs
@@ -1,7 +1,16 @@
-﻿namespace atheneum_app.DataAccess.Interfaces
+﻿using System.Threading.Tasks;
+using atheneum_app.Models.Binding;
+using atheneum_app.Models.View;
+using Refit;
+
+namespace atheneum_app.DataAccess.Interfaces
 {
     public interface IAuthService
     {
-        
+        [Post("/auth/login")]
+        Task<AuthViewModel> Login([Body] LoginBindingModel payload);
+
+        [Post("/auth/register")]
+        Task<AuthViewModel> Register([Body] RegisterBindingModel payload);
     }
 }
