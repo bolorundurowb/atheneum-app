3341e72ddd150df423125d54016ccef141f69bb6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-08 12:13:01 +0100

    ft: add in binding models

---
+++ atheneum-app/atheneum-app/Models/Binding/BorrowBookBindingModel.cs
@@ -0,0 +1,7 @@
+﻿namespace atheneum_app.Models.Binding
+{
+    public class BorrowBookBindingModel
+    {
+        public string BorrowerName { get; set; }
+    }
+}
---
+++ atheneum-app/atheneum-app/Models/Binding/RegisterBindingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace atheneum_app.Models.Binding
+{
+    public class RegisterBindingModel
+    {
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+    }
+}
