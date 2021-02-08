8afaada8edc2bbc8e1258435f66e9b5dabc78966
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-08 12:26:05 +0100

    ft: add in the view model

---
+++ atheneum-app/atheneum-app/Models/View/BookViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace atheneum_app.Models.View
+{
+    public class BookViewModel
+    {
+        public string _id { get; set; }
+
+        public string ExternalId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Isbn { get; set; }
+
+        public string Isbn13 { get; set; }
+
+        public string CoverArt { get; set; }
+
+        public int? PublishYear { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public List<BorrowingHistoryItem> BorrowingHistory { get; set; }
+    }
+}
---
+++ atheneum-app/atheneum-app/Models/View/PublisherViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace atheneum_app.Models.View
+{
+    public class PublisherViewModel
+    {
+        public string _id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
