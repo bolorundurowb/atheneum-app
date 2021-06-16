81e82b9a463daac8df15775c56c1afa8b9561825
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-16 15:28:24 +0100

    ft: finalize the styling for the back button

--- atheneum-app/atheneum-app/App.xaml.cs
+++ atheneum-app/atheneum-app/App.xaml.cs
@@ -5,7 +5,10 @@ using Xamarin.Forms.Xaml;
 
 [assembly: ExportFont("proxima.otf", Alias = "Proxima")]
 [assembly: ExportFont("fredericka.ttf", Alias = "Fredericka")]
+[assembly: ExportFont("faregular.otf", Alias = "FAR")]
+[assembly: ExportFont("fasolid.otf", Alias = "FAS")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
 namespace atheneum_app
 {
     public partial class App : Application
@@ -19,6 +22,7 @@ namespace atheneum_app
             var isLoggedIn = tokenClient.IsLoggedIn();
 
             NavigationPage mainPage;
+
             if (isLoggedIn)
             {
                 mainPage = new NavigationPage(new MainPage())
