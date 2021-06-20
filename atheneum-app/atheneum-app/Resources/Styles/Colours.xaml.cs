ddf22da4f40d926627807aa7fad4fbe05d6b9421
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-21 00:25:03 +0100

    ft: add in colours xaml

---
+++ atheneum-app/atheneum-app/Resources/Styles/Colours.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace atheneum_app.Resources.Styles
+{
+    public partial class Colours : ResourceDictionary
+    {
+        public Colours()
+        {
+            InitializeComponent();
+        }
+    }
+}
