435b8f2aecf22a8aa28e50be7ef05cead06f075a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-23 18:32:34 +0100

    ft: add in fields to auth response

--- src/atheneum-app.Library/Models/View/AuthViewModel.cs
+++ src/atheneum-app.Library/Models/View/AuthViewModel.cs
@@ -2,7 +2,9 @@
 {
     public class AuthViewModel
     {
-        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
 
         public string AuthToken { get; set; }
 
