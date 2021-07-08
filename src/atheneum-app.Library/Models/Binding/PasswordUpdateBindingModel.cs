8adebe855fd6dd0f4f557b097bdcb73f95423b40
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-08 17:14:05 +0100

    ft: add api helper methods

---
+++ src/atheneum-app.Library/Models/Binding/PasswordUpdateBindingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace atheneum_app.Library.Models.Binding
+{
+    public class PasswordUpdateBindingModel
+    {
+        public string CurrentPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
