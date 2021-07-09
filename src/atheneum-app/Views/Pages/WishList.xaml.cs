a99eef4a07e91ab4ec085a051a196abf49ec0bc9
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-09 14:43:28 +0100

    ft: add wishlist page

---
+++ src/atheneum-app/Views/Pages/WishList.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace atheneum_app.Views.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WishList : ContentView
+    {
+        public WishList()
+        {
+            InitializeComponent();
+        }
+    }
+}
