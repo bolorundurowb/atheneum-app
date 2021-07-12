f71bb66a7759ea9b072b41a0de16453f33079588
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-12 20:16:46 +0100

    ft: add missing code behind file

---
+++ src/atheneum-app/Views/Pages/Scanner.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Mobile;
+
+namespace atheneum_app.Views.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Scanner : ContentPage
+    {
+        public string ScanResult { get; private set; }
+        
+        public Scanner()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            scanner.Options = new MobileBarcodeScanningOptions
+            {
+               AutoRotate = false
+            };
+        }
+
+        private void OnScan(Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                ScanResult = result?.Text;
+                await Navigation.PopAsync();
+            });
+        }
+    }
+}
