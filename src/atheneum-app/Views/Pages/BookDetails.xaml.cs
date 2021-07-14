53b1b16a8f519e7560d052539dfaa21b0fd5d8b8
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-14 17:34:42 +0100

    ft: add constructor details for the view model

--- src/atheneum-app/Views/Pages/BookDetails.xaml.cs
+++ src/atheneum-app/Views/Pages/BookDetails.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using atheneum_app.Library.Models.View;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +7,18 @@ namespace atheneum_app.Views.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BookDetails : ContentPage
     {
-        public BookDetails()
+        private readonly BookViewModel _book;
+
+        public BookDetails(BookViewModel book)
         {
             InitializeComponent();
+            _book = book;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            BindingContext = _book;
         }
     }
 }
