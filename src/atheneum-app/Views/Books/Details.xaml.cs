9da21998c8a184f6aadde1587586a2782d0d5ba4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-15 23:10:58 +0100

    ft: add remove button handling logic

--- src/atheneum-app/Views/Books/Details.xaml.cs
+++ src/atheneum-app/Views/Books/Details.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using atheneum_app.Library.DataAccess.Implementations;
 using atheneum_app.Library.Models.View;
+using atheneum_app.Utils;
+using Refit;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,18 +11,38 @@ namespace atheneum_app.Views.Books
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Details : ContentPage
     {
+        private readonly BookService _bookService;
         private readonly BookViewModel _book;
 
         public Details(BookViewModel book)
         {
             InitializeComponent();
             _book = book;
+            _bookService = new BookService();
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
             BindingContext = _book;
+        }
+
+        protected async void Remove(object sender, EventArgs e)
+        {
+            const string genericErrorMessage =
+                "Sorry, an error occurred when removing from your library. Try again later.";
+
+            try
+            {
+                await _bookService.Remove(_book.Id);
+                ToastService.Success("Book successfully removed from your library.");
+                await Navigation.PopAsync();
+            }
+            catch (ApiException ex)
+            {
+                var error = await ex.GetContentAsAsync<ErrorViewModel>();
+                ToastService.Error(error?.Message?.Length > 0 ? error.Message : genericErrorMessage);
+            }
         }
 
         protected async void GoBack(object sender, EventArgs e)
