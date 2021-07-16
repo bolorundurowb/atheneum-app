0553ce7cf8e749d909cd4e5c06d651c186bd58c4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-16 10:12:47 +0100

    ft: add in new view models

---
+++ src/atheneum-app.Library/Models/View/TopAuthorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace atheneum_app.Library.Models.View
+{
+    public class TopAuthorViewModel
+    {
+        public AuthorViewModel Author { get; set; }
+
+        public int NumOfBooks { get; set; }
+    }
+}
