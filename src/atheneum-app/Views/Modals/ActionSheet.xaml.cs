5a8a585bfbbece2d6f1d01de49eaf491ab56e6c1
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-18 22:47:50 +0100

    ft: add in shared styling and action handlers

--- src/atheneum-app/Views/Modals/ActionSheet.xaml.cs
+++ src/atheneum-app/Views/Modals/ActionSheet.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using atheneum_app.Library.Enums;
 using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms.Xaml;
 
@@ -12,7 +13,22 @@ namespace atheneum_app.Views.Modals
             InitializeComponent();
         }
 
-        private void Button_OnClicked(object sender, EventArgs e)
+        private void Scan(object sender, EventArgs e)
+        {
+            Dismiss(ActionType.ByScan);
+        }
+
+        private void Isbn(object sender, EventArgs e)
+        {
+            Dismiss(ActionType.ByIsbn);
+        }
+
+        private void Manual(object sender, EventArgs e)
+        {
+            Dismiss(ActionType.Manual);
+        }
+
+        private void Cancel(object sender, EventArgs e)
         {
             Dismiss(null);
         }
