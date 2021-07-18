5e0c8a4f7b6acea78bb6cd51c562093ea47003c5
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-18 22:31:41 +0100

    ft: add in action sheet modal scaffold

---
+++ src/atheneum-app/Views/Modals/ActionSheet.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms.Xaml;
+
+namespace atheneum_app.Views.Modals
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActionSheet : Popup
+    {
+        public ActionSheet()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Dismiss(null);
+        }
+    }
+}
