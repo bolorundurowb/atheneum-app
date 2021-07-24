e7fd083fea8c5b4454dbb7fa15caa2b44685c834
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-24 19:22:41 +0100

    ft: add modal scaffold

---
+++ src/atheneum-app/Views/Modals/ManualBookEntry.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using atheneum_app.Library.DataAccess.Implementations;
+using atheneum_app.Library.Models.View;
+using atheneum_app.Utils;
+using Refit;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms.Xaml;
+
+namespace atheneum_app.Views.Modals
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManualBookEntry : Popup
+    {
+        private readonly BookService _bookService;
+        
+        public ManualBookEntry()
+        {
+            InitializeComponent();
+            _bookService = BookService.Instance();
+        }
+
+        protected async void Add(object sender, EventArgs e)
+        {
+            const string genericErrorMessage =
+                "Sorry, an error occurred when adding the book to your library. Try again later.";
+            var isbn = txtIsbn.Text;
+
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                ToastService.Error("An isbn is required.");
+                return;
+            }
+
+            stkBtns.IsVisible = false;
+            prgAdding.IsVisible = true;
+
+            try
+            {
+                 await _bookService.AddByIsbn(isbn);
+                
+                // notify user
+                ToastService.Success("Book successfully added to your library.");
+                
+                // send data back
+                Dismiss("");
+            }
+            catch (ApiException ex) when (ex.StatusCode is HttpStatusCode.BadRequest)
+            {
+                var error = await ex.GetContentAsAsync<ValidationErrorViewModel>();
+
+                ToastService.Error(error?.Message?.Length > 0 ? error.Message[0] : genericErrorMessage);
+            }
+            catch (ApiException ex)
+            {
+                var error = await ex.GetContentAsAsync<ErrorViewModel>();
+
+                ToastService.Error(!string.IsNullOrWhiteSpace(error?.Message) ? error.Message : genericErrorMessage);
+            }
+            finally
+            {
+                stkBtns.IsVisible = true;
+                prgAdding.IsVisible = false;
+            }
+        }
+
+        protected void Cancel(object sender, EventArgs e)
+        {
+            Dismiss(null);
+        }
+
+        protected override object GetLightDismissResult()
+        {
+            return null;
+        }
+    }
+}
