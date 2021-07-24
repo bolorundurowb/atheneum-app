4e30f47329baf487e47c7c3a248f4b245304ea04
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-24 15:04:43 +0100

    ft: update the binding model

--- src/atheneum-app.Library/Models/Binding/CreateManualBookBindingModel.cs
+++ src/atheneum-app.Library/Models/Binding/CreateManualBookBindingModel.cs
@@ -5,23 +5,15 @@
         public string Title { get; set; }
 
         public string Summary { get; set; }
-        
+
         public string Isbn { get; set; }
 
-        public string Isbn13 { get; set; }
-        
         public int? PublishYear { get; set; }
 
-        public string[] AuthorIds { get; set; }
-        
-        public string PublisherId { get; set; }
+        public string Authors { get; set; }
 
-        public string CoverArt { get; set; }
+        public string Publisher { get; set; }
 
         public int? PageCount { get; set; }
-        
-        public double? Longitude { get; set; }
-
-        public double? Latitude { get; set; }
     }
 }
