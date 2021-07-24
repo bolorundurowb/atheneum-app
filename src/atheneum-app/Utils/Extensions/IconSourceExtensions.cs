3943e67dbf1e28071aa480b920c037e9964250ad
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-24 23:53:07 +0100

    ft: add icon source extensions

--- src/atheneum-app/Utils/Extensions/IconSourceExtensions.cs
+++ src/atheneum-app/Utils/Extensions/IconSourceExtensions.cs
@@ -1,45 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace atheneum_app.Utils.Extensions
 {
-    [ContentProperty(nameof(Source))]
-    public class IconSourceExtensions : IMarkupExtension
+    public class IconSourceExtensions : BaseSourceExtensions
     {
         private static readonly Dictionary<string, ImageSource> Cache = new Dictionary<string, ImageSource>();
 
-        public string Source { get; set; }
+        protected override string ResourceFolder => "Icons";
 
-        public static ImageSource GetImageSource(string value)
-        {
-            if (value == null)
-            {
-                return null;
-            }
+        public override string ResourcePrefix => "ic";
 
-            var resourceKey = "atheneum_app.Resources.Icons." + value + ".png";
-
-            if (Cache.ContainsKey(resourceKey))
-            {
-                return Cache[resourceKey];
-            }
-
-            var imageSource = ImageSource.FromResource(resourceKey);
-            Cache.Add(resourceKey, imageSource);
-
-            return imageSource;
-        }
-
-        public object ProvideValue(IServiceProvider serviceProvider)
-        {
-            if (Source == null)
-            {
-                return null;
-            }
-
-            return GetImageSource(Source);
-        }
+        public override string ResourceSuffix => ".png";
     }
 }
