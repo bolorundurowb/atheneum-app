8a11dea36b0884d50bce5753170fd9f386efe4cf
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-02-24 20:26:07 +0100

    ft: rename tab folders

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,15 +9,15 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../home/tab1.module').then(m => m.Tab1PageModule)
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../library/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'wishlist',
+        loadChildren: () => import('../wishlist/wishlist.module').then(m => m.WishListModule)
       },
       {
         path: '',
