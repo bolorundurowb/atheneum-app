afa5f1dd40a2b7e123ed23f533b4bac4d18b61ba
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-02-26 11:24:26 +0100

    ft: add in the auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { asPromise } from '../utils';
+
+@Injectable({ providedIn: 'root'})
+export class AuthService {
+  userKey = 'ath-user';
+  private readonly baseUrl;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = `${environment.baseApiUrl}/v1/auth`;
+  }
+
+  login(user: any): Promise<any> {
+    return asPromise(this.http.post<any>(`${this.baseUrl}/login`, user));
+  }
+
+  register(user: any): Promise<any> {
+    return asPromise(this.http.post<any>(`${this.baseUrl}/register`, user));
+  }
+
+  forgotPassword(payload: any): Promise<any> {
+    return asPromise(this.http.post<any>(`${this.baseUrl}/forgot-password`, payload));
+  }
+
+  resetPassword(payload: any): Promise<any> {
+    return asPromise(this.http.post<any>(`${this.baseUrl}/reset-password`, payload));
+  }
+
+  verifyEmail(payload: any): Promise<any> {
+    return asPromise(this.http.post<any>(`${this.baseUrl}/verify-email`, payload));
+  }
+
+  resendVerificationCode(payload: any): Promise<any> {
+    return asPromise(this.http.post<any>(`${this.baseUrl}/resend-verification-code`, payload));
+  }
+
+  isLoggedIn() {
+    return !!localStorage.getItem(this.userKey);
+  }
+
+  logout() {
+    localStorage.removeItem(this.userKey);
+  }
+
+  persistUser(user: any): void {
+    localStorage.setItem(this.userKey, JSON.stringify(user));
+  }
+
+  getUser(): any {
+    return JSON.parse(localStorage.getItem(this.userKey)!);
+  }
+
+  getToken(): string {
+    return this.getUser().authToken;
+  }
+}
