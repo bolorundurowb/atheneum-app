aad4b5f746e60a9bf3a831378810eb48a67c42a6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-03-13 12:26:53 +0100

    ft: fix the book and wishlist display

--- src/app/shared/book/book.component.ts
+++ src/app/shared/book/book.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
     <div class="book">
       <img [src]="book.coverArt">
       <div class="title">{{book.title}}</div>
-      <div class="author">{{ book.authors[0].name }}</div>
+      <div class="author">{{ book.authors ? book.authors[0]?.name : book.authorName }}</div>
     </div>
   `,
   styleUrl: './book.component.scss'
