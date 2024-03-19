6c8f0c3f919beace4a3644fa363630c5209235c4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-03-19 11:16:13 +0100

    ft: finalize the logout support

--- src/app/tabs/settings/settings.page.ts
+++ src/app/tabs/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: [ 'settings.page.scss' ]
 })
 export class SettingsPage {
-  constructor() {
+  logOutButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+      }
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: async () => {
+        this.authService.logout();
+        await this.router.navigate([ 'auth', 'login' ]);
+      }
+    }
+  ];
+
+  constructor(private authService: AuthService, private router: Router) {
   }
 }
