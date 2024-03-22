a7620e12e9e09fc04e8a4ab8be52d7568cbc5851
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-03-22 13:38:10 +0100

    ft: add in author and publisher services

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,5 +1,7 @@
 export * from './auth.service';
+export * from './author.service';
 export * from './book.service';
 export * from './notification.service';
+export * from './publisher.service';
 export * from './user.service';
 export * from './wishlist.service';
