cc839d962802a8545d27a947970ab2c6ebd290fb
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-03-22 13:44:21 +0100

    ft: add in init service calls

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorService, BookService, NotificationService, PublisherService } from '../../services';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: [ 'home.page.scss' ]
 })
-export class HomePage {
-  constructor() {
+export class HomePage implements OnInit {
+  isLoading = false;
+  recentBooks: any[] = [];
+  topAuthors: any[] = [];
+  topPublishers: any[] = [];
+
+  constructor(private bookService: BookService, private authorService: AuthorService, private publisherService: PublisherService,
+    private notificationService: NotificationService) {
+  }
+
+  async ngOnInit() {
+    this.isLoading = true;
+
+    try {
+      this.recentBooks = await this.bookService.getRecent();
+      this.topAuthors = await this.authorService.getTop();
+      this.topPublishers = await this.publisherService.getTop();
+    } catch (e) {
+      await this.notificationService.error(e as string);
+    } finally {
+      this.isLoading = false;
+    }
   }
 }
