8489d7871798811d61bf88ffdbbc30606f609d1e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-03-24 19:49:25 +0100

    ft: add page details scaffold

---
+++ src/app/details/book/book.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: 'book.page.html',
+  styleUrl: 'book.page.scss'
+})
+export class BookPage {
+}
