ee4af641a28938f71d0c045b8e12c31b24ba08f5
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-03-24 22:59:32 +0100

    ft: add book removal support

--- src/app/details/book/book.page.ts
+++ src/app/details/book/book.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { BookService, NotificationService } from '../../services';
 
 @Component({
   selector: 'app-book-details',
@@ -9,8 +10,30 @@ import { NavController } from '@ionic/angular';
 })
 export class BookPage implements OnInit {
   book: any;
+  removeButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+      }
+    },
+    {
+      text: 'Proceed',
+      role: 'confirm',
+      handler: async () => {
+        try {
+          await this.bookService.removeBook(this.book.id);
+          await this.notificationService.success('Book successfully removed');
+          await this.goBack();
+        } catch (e) {
+          await this.notificationService.error(e as string);
+        }
+      }
+    }
+  ];
 
-  constructor(private route: ActivatedRoute, private navCtrl: NavController) {
+  constructor(private route: ActivatedRoute, private navCtrl: NavController,
+              private notificationService: NotificationService, private bookService: BookService) {
   }
 
   ngOnInit() {
