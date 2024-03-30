860095841428bf70f750aefc024bab6020459118
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-03-30 23:35:20 +0100

    ft: add in action sheet

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: [ 'tabs.page.scss' ]
 })
 export class TabsPage {
+  public actionSheetButtons = [
+    {
+      text: 'scan barcode',
+      data: {
+        action: 'scan-isbn',
+      },
+    },
+    {
+      text: 'enter isbn',
+      data: {
+        action: 'manual-isbn',
+      },
+    },
+    {
+      text: 'manually enter',
+      data: {
+        action: 'manual-entry',
+      },
+    },
+    {
+      text: 'close',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
   constructor() {
   }
 }
