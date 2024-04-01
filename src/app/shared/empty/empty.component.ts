bb7e718f6c7ca421509b8f7100013fdadd9928b2
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-04-01 06:54:44 +0100

    ft: add in empty component scaffold

---
+++ src/app/shared/empty/empty.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-empty',
+  template: `
+  <div>Hello</div>
+  `,
+  styleUrl: './empty.component.scss'
+})
+export class EmptyComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BookComponent } from './book/book.component';
 import { CommonModule } from '@angular/common';
+import { EmptyComponent } from './empty/empty.component';
 
 @NgModule({
   imports: [ CommonModule ],
   exports: [
-    BookComponent
+    BookComponent,
+    EmptyComponent,
   ],
-  declarations: [ BookComponent ]
+  declarations: [ BookComponent, EmptyComponent ]
 })
-export class SharedModule {}
+export class SharedModule {
+}
