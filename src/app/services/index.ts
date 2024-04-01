6f750f4ddd7f2d11b1d61c9f3d131206e9333919
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-04-01 15:19:08 +0100

    ft: load stats on home page

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -3,5 +3,6 @@ export * from './author.service';
 export * from './book.service';
 export * from './notification.service';
 export * from './publisher.service';
+export * from './statistic.service';
 export * from './user.service';
 export * from './wishlist.service';
